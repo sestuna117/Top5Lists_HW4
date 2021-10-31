7fea974f5317726fbe7737d2ff8005fe1b5e3bcc
JGuo <jason.guo2560@gmail.com>
2021-10-31 16:24:01 -0400

    v1.0.2 - implemented more foolproof designs

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -50,6 +50,7 @@ const HomeScreen = () => {
                     aria-label="add"
                     id="add-list-button"
                     onClick={handleCreateNewList}
+                    disabled={store.isListNameEditActive}
                 >
                     <AddIcon />
                 </Fab>
